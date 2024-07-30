4876f02022e66d6d85c9699f94e968c4e5d0eeb8
sankalpaexperiments <146099641+sankalpaexperiments@users.noreply.github.com>
2024-07-30 18:29:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Sankalpa App")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building!."
 )
